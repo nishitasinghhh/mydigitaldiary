c1f163e61dd9701365400677a1d958f8d5446f1d
Nishita <nishitasingh022@gmail.com>
2024-07-02 13:32:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route,Link} from 'react-router-dom'
 import './App.css';
 import {auth} from '/Users/nishitasingh/Desktop/mydigitaldiary/src/firebase-config.js'
 import Home from './pages/Home';
